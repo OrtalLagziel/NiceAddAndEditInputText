b837cca388d97a1ecfbf9cf40b3ccefc54cbe551
ortalla <ortal.lagziel@nice.com>
2016-07-13 09:35:48 +0300

    test

---
+++ dist/NiceAddAndEditInputTextPO.js
@@ -0,0 +1,13 @@
+function NiceAddAndEditInputTextPO() {
+    'use strict';
+
+    var elements = {
+    };
+
+    var EC = protractor.ExpectedConditions;
+
+    /*jshint validthis: true */
+    var self = this;
+}
+
+module.exports =  NiceAddAndEditInputTextPO;
